fd110f3eb829ae5eeadc2fb5db9821c3ddbbb408
markol-vertex <marko.lahteenmaki@vertex.fi>
2018-06-08 18:08:38 +0300

    added firebase info

---
+++ cards/firebase.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/5.0.4/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA_2fHjzIFCOq3z36hnV0umn7pw86YjJY8",
+    authDomain: "vertex-lottery.firebaseapp.com",
+    databaseURL: "https://vertex-lottery.firebaseio.com",
+    projectId: "vertex-lottery",
+    storageBucket: "vertex-lottery.appspot.com",
+    messagingSenderId: "48350714118"
+  };
+  firebase.initializeApp(config);
+</script>
