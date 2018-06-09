304e5f06949bfb61f6261684defe32fed917bbd7
MMinGit <mark0@live.fi>
2018-06-09 11:14:36 +0300

    lottery locks

--- cards/src/app/draw-lots.service.ts
+++ cards/src/app/draw-lots.service.ts
@@ -1,18 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Map } from './map';
 import { Card } from './card';
 import { CardService } from './card.service';
+import { LotteryLock } from './lottery-lock';
+import { MessageService } from './message.service';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({  providedIn: 'root'})
 export class DrawLotsService {
   private cards: Card[];
-  constructor(private cardService: CardService) {this.cards=[]; }
 
-  drawLots() : boolean{
+  private lockUrl = 'api/lottery';  // URL to web api
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService,
+    private cardService: CardService
+  ) {this.cards=[]; }
+
+  drawLots() : Boolean{
     let R=false;
     this.cardService.getCards().subscribe((acards : Card[]) => {
       this.cards = JSON.parse(JSON.stringify(acards));
@@ -34,6 +48,39 @@ export class DrawLotsService {
     }
   });
   return R;
+  }
+
+    /** POST price by id. Will 404 if id not found */
+    setLock(lock: LotteryLock) : Observable<any> {
+      return this.http.post(this.lockUrl, lock, httpOptions).pipe(
+        tap(_ => this.log(`updated lock ${lock}`)),
+        catchError(this.handleError<any>('setLock'))
+      );
+    }
+
+      /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log a CardService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add('PriceService: ' + message);
   }
 
 }
