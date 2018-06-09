8f486f7d2203a0802023593ffa8fbfa5ed8e14af
MMinGit <mark0@live.fi>
2018-06-09 11:47:17 +0300

    updated reset

--- cards/src/app/lottery-lock.ts
+++ cards/src/app/lottery-lock.ts
@@ -1,2 +1,5 @@
 export class LotteryLock {
+    id: number;
+    name: string;
+    lock: number;
 }
--- cards/src/app/reset-pin/reset-pin.component.ts
+++ cards/src/app/reset-pin/reset-pin.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { Card } from '../card';
 import { CardService } from '../card.service';
+import { DrawLotsService } from '../draw-lots.service';
+import { LotteryLock } from '../lottery-lock';
 
 @Component({
   selector: 'app-reset-pin',
@@ -12,7 +14,10 @@ export class ResetPinComponent implements OnInit {
   pinValue : string;
   cards: Card[];
 
-  constructor(private cardService: CardService) { }
+  constructor(
+    private cardService: CardService,
+    private drawLotsService: DrawLotsService
+  ) { }
 
   ngOnInit() {
   }
@@ -21,12 +26,20 @@ export class ResetPinComponent implements OnInit {
     if(this.pinValue == '1234'){
       this.cardService.getCards().subscribe((acards : Card[]) => {
         this.cards = JSON.parse(JSON.stringify(acards));
-      for (var i = 0; i < this.cards.length; i++) {
-        this.cards[i].rnd = undefined;
-        this.cards[i].owner = undefined;
-        this.cardService.updateCard (this.cards[i]).subscribe();
-      }
-    });
+        for (var i = 0; i < this.cards.length; i++) {
+          this.cards[i].rnd = undefined;
+          this.cards[i].owner = undefined;
+          this.cardService.updateCard (this.cards[i]).subscribe();
+        }
+      });
+      this.drawLotsService.getLock('lottery-start').subscribe((lock : LotteryLock) => {
+        lock.lock = 0;
+        this.drawLotsService.setLock(lock).subscribe();
+      });
+      this.drawLotsService.getLock('lottery-done').subscribe((lock : LotteryLock) => {
+        lock.lock = 1;
+        this.drawLotsService.setLock(lock).subscribe();
+      });
     }
   }
 
